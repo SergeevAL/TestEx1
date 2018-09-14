c6efaee48d7eb650ad8e8c891274af62d1966193
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2018-09-14 12:50:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,134 @@
 <?php
 
 class Objects{
+    /**
+     * 
+     * Данные из базы
+     * @name string Имя
+     * @status int Статус
+     * 
+     */
+
     public static $name = 'Alex';
     public static $status = 1;
 }
 
 final class Item {
+    /**
+     * 
+     * @id int Идендификатор 
+     * @name string Имя item 
+     * @status int Стастус item
+     * @changed bool Определение изменения данных name и статус
+     * 
+     */
+
     private $id = NULL;
     private $name;
     private $status;
     private $changed;
 
+     /**
+     * 
+     * Конструктор проверяет id на принадлежность к типу int
+     * Потом присваивает свойствам класса Item значения id и changed
+     * @param int $id индендификатор
+     * 
+     */
     public function __construct($id)
     {
+        if(!is_int($id))
+        {
+            die("Id not int");
+        }
+        
         $this->id = $id;
         $this->changed = false;
         echo 'Конструктор <br>';
         $this->init();
     }
 
+    /**
+         * 
+         * Функция инициализации получения данных из базы
+         * 
+         */
     private function init()
     {
-        if ($this->id != NULL)
-        {
-            $this->name = Objects::$name;
-            $this->status = Objects::$status;
-            echo 'Инициализировано <br>';
+        try {
+            if ($this->id !== NULL)
+            {
+                if(!is_string(Objects::$name) || !is_int(Objects::$status))
+                {
+                    die("Wrong object");
+                }
+                $this->name = Objects::$name;
+                $this->status = Objects::$status;
+                echo 'Инициализировано <br>';
+            }
+            else{
+                throw new Exception ('Уже было инициализировано');
+            }
         }
-        else {
-            echo 'Уже было инициализировано<br>';
+        catch (Exception $e) {
+            echo $e->getMessage();
         }
     }
 
+    /**
+     * 
+     * Установка нового значения для name или status
+     * @param string $property, Свойство класса Item кроме Id
+     * @param int,string $value, Значение
+     * 
+     */
+    
     public function __set($property, $value) 
     {
-        if (property_exists($this, $property) & $property != 'id' & $value != NULL) {
+        try {
+            if (property_exists($this, $property) & $property !== 'id' & $value !== NULL) {
                 $this->$property = $value;
                 $this->changed = true;
                 echo ' Значение ' . $property . ' заменено на ' . $value . ' <br> ';
+            }
+            else{
+                throw new Exception ('Ошибка доступа для __set, неверное свойство класса.');
+            }
         }
-        else{
-            echo ' Ошибка доступа для __set, неверное свойство класса.<br>';
+        catch (Exception $e) {
+            echo $e->getMessage();
         }
     }
+
+    /**
+     * 
+     * Получения свойства класса name или status
+     * @param string $property свойства класа кроме id
+     * 
+     */
 
     public function __get($property) 
     {
-        if (property_exists($this, $property)) {
+        try {
+            if (property_exists($this, $property)) {
 
-            echo $this->$property . ' <br> ';
-            return $this->$property;
+                echo $this->$property . ' <br> ';
+                return $this->$property;
+            }
+            else{
+                throw new Exception ('Ошибка доступа для __get, неверное свойство класса.');
+            }
         }
-        echo ' Ошибка доступа для __get, неверное свойство класса.<br> ';
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
+
+    /**
+     * 
+     * Сохранение даных в базу
+     * 
+     */
 
     public function save()
     {
@@ -67,6 +143,12 @@ final class Item {
         }
     }
 }
+
+/**
+ * 
+ * Для проверки
+ * 
+ */
 
 $var = new Item(1);
 
